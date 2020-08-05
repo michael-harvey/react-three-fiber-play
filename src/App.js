f9bd67e2a3fba3e175969689aaaf1deb51d24068
Mike Harvey <harvey.miche@gmail.com>
2020-08-05 13:32:16 +1200

    Add basic box example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+
+function Box(props) {
+  const mesh = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? "hotpink" : "pink"}
+      />
+    </mesh>
+  );
+}
+
+function Scene() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  return (
+    <>
+      <ambientLight />
+      <pointLight intensity={0.3} position={[10, 10, 10]} />
+      <Box position={[0, -1.2, 1]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <Box position={[0, 1.2, 1]} />
+      <orbitControls args={[camera, domElement]} />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
 }
 
