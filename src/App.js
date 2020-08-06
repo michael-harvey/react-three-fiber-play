c9ae9c261077c2e097f3ab948b86c1b8b55e1672
Mike Harvey <harvey.miche@gmail.com>
2020-08-06 16:17:12 +1200

    Add box animated by react spring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from "react";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
-import { HTML, OrbitControls } from "drei";
+import { OrbitControls } from "drei";
+import { useSpring } from "@react-spring/core";
+import { a } from "@react-spring/three";
 
 function Box(props) {
   const mesh = useRef();
@@ -8,26 +10,54 @@ function Box(props) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  useFrame(() => {
+    const rot = props.speed * 0.01;
+    mesh.current.rotation.x += rot;
+    mesh.current.rotation.y += rot;
+  });
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
+    <>
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshStandardMaterial
+          attach="material"
+          color={hovered ? "hotpink" : "pink"}
+        />
+      </mesh>
+    </>
+  );
+}
+
+function BouncyBox() {
+  const [active, setActive] = useState(0);
+
+  const { spring } = useSpring({
+    spring: active,
+    config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
+  });
+
+  // interpolate values from commong spring
+  const scale = spring.to([0, 1], [1, 5]);
+  const rotation = spring.to([0, 1], [0, Math.PI]);
+  const color = spring.to([0, 1], ["#6246ea", "#e45858"]);
+  return (
+    <a.mesh
+      rotation-y={rotation}
+      scale-x={scale}
+      scale-z={scale}
+      onClick={() => setActive(Number(!active))}
     >
-      <HTML>
-        <h1>Hello</h1>
-      </HTML>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? "hotpink" : "pink"}
-      />
-    </mesh>
+      <a.meshStandardMaterial roughness={0.5} attach="material" color={color} />
+    </a.mesh>
   );
 }
 
@@ -40,11 +70,12 @@ function Scene() {
     <>
       <ambientLight />
       <pointLight intensity={0.3} position={[10, 10, 10]} />
-      <Box position={[0, -1.2, 1]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <Box position={[0, 1.2, 1]} />
+      <Box position={[0, -1.2, 1]} speed={1} />
+      <Box position={[-1.2, 0, 0]} speed={2} />
+      <Box position={[1.2, 0, 0]} speed={3} />
+      <Box position={[0, 1.2, 1]} speed={4} />
       <OrbitControls args={[camera, domElement]} />
+      <BouncyBox />
     </>
   );
 }
